39902907283e5d9c2264807e1ee9cf6f0703b4ae
PrettyMagnolia <109959622+PrettyMagnolia@users.noreply.github.com>
2022-12-18 12:46:12 +0800

    修改销量实体名，并添加组合查询接口

--- src/main/java/com/liuyifei/hive/entity/OrderInfo.java
+++ src/main/java/com/liuyifei/hive/entity/OrderInfo.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TopOrder {
+public class OrderInfo {
     String product_id;
     Integer sale_num;
     String sum_pay;
